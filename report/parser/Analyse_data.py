bf271a083f057b97655a2d781933a25a081c4e29
Darren Daly <archerydwd@gmail.com>
2014-03-29 18:24:10 +0000

    Add the Analyse Data module
    Change-Id: I7a6400fca3675560f12b1fb148f3e463034acea0

---
+++ report/parser/Analyse_data.py
@@ -0,0 +1,19 @@
+'''
+python 2.6
+Created on 29 Mar 2014
+@version: 1.0.0
+@author: darrendaly
+'''
+
+class AnalyseData:
+    '''
+    class to analyse data produced by the report class
+    '''
+    
+    def __init__(self):
+        '''
+        Constructor to initialise the new AnalyseData object
+        '''
+        
+    
+        
