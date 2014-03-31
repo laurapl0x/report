40f79e1bd47cc8858295e56a74faffb62e7aecf0
DarrenDaly <Darren.Daly@adtech.com>
2014-03-31 12:50:42 +0100

    changes to report

--- report/parser/report.py
+++ report/parser/report.py
@@ -58,43 +58,6 @@ class Report:
 
 
 
-    def calculateAverages(self):
-        '''Calculates the average value of occurrences'''
-        for each_line in self._data:
-            total = 0
-            size = len(each_line['datapoints'])
-            for counter in range(size):
-                total += each_line['datapoints'][counter][0]
-                avg = total / size
-            print each_line['target'] + " average value is: " + str(avg) + " and the total value is: " + str(total)
-    
-    def findMins(self):
-        '''find the minimum value in each section'''
-        for each_line in self._data:
-            size = len(each_line['datapoints'])
-            for counter in range(size):
-                value = each_line['datapoints'][counter][0]
-                if counter == 0:
-                    minimum = value
-                elif minimum > value:
-                    minimum = value
-            print each_line['target'] + " minimum value = " + str(minimum)
-    
-    def findMaxs(self):
-        '''find the maximum value in each section'''
-        for each_line in self._data:
-            size = len(each_line['datapoints'])
-            for counter in range(size):
-                value = each_line['datapoints'][counter][0]
-                if counter == 0:
-                    maximum = value
-                elif maximum < value:
-                    maximum = value
-            print each_line['target'] + " maximum value = " + str(maximum)
-            
+   
 report = Report()      
-report.calculateAverages()
-print ""
-report.findMins()
-print ""
-report.findMaxs()
+
